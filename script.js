7c9c0e9c71224806897e29a8c8af770b5008d23b
petraalpigiano <petraalpigiano@gmail.com>
2025-04-15 16:39:46 +0200

    add members

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
 // (trovate l’array del team all’interno della cartella in allegato)
+const rowHTML = document.getElementById("row");
 
 const teamMembers = [
   {
@@ -39,3 +40,19 @@ const teamMembers = [
     img: "img/female3.png",
   },
 ];
+
+let cartHTML = ``;
+for (const member of teamMembers) {
+  cartHTML += `
+    <div class="col">
+      <div class="card h-100">
+        <img src="${member.img}" class="card-img-top" alt="${member.name}" />
+        <div class="card-body">
+          <h5 class="card-title">${member.name}</h5>
+          <p class="card-text mb-2">${member.role}</p>
+          <p><a href="${member.email}" class="link">${member.email}</a></p>
+        </div>
+      </div>
+    </div>`;
+}
+rowHTML.innerHTML = cartHTML;
